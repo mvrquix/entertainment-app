a5df6b9cc0a32aea1465f8b3176d8a7be7ea3bf9
Marquis Morrow <quis.morrow@gmail.com>
2023-03-17 08:45:49 -0700

    initial commit fixes

--- README.md
+++ README.md
@@ -1,70 +1,70 @@
-# Getting Started with Create React App
+# Frontend Mentor - Entertainment web app solution
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a solution to the [In-browser markdown editor challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/inbrowser-markdown-editor-r16TrrQX9). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
-## Available Scripts
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Navigate between Home, Movies, TV Series, and Bookmarked Shows pages
+- Add/Remove bookmarks from all movies and TV series
+- Search for relevant shows on all pages
+- **Bonus**: Build this project as a full-stack application
+- **Bonus**: If you're building a full-stack app, we provide authentication screen (sign-up/login) designs if you'd like to create an auth flow
+
+### Screenshot
+
+![](./screenshot.png)
+
+## Run Project Locally
 
 In the project directory, you can run:
 
-### `npm start`
+### `npm install`
+
+to download dependencies.
+
+### `npm run start`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+### Links
 
-### `npm test`
+- Solution URL: [https://github.com/mvrquix/markdown-editor](https://github.com/mvrquix/markdown-editor)
+- Live Site URL: [https://isaqc-entertainment.web.app/](https://isaqc-entertainment.web.app)
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## My process
 
-### `npm run build`
+### Built with
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+- [Bootstrap](https://getbootstrap.com/docs/5.0/getting-started/introduction)
+- Desktop-first workflow
+- [React](https://reactjs.org/) - JS library
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+### Continued development
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+The next step to this project would be to create a server application to persist data with endpoints for the frontend to network with. With that in place
+it would make sense to centralize state management on the frontend in a redux store.
 
-### `npm run eject`
+## Author
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- Website - [Marquis Morrow](https://isaqc.io/)
+- Frontend Mentor - [@mvrquix](https://www.frontendmentor.io/profile/mvrquix)
+- Linkedin - [@marquismorrow](https://www.linkedin.com/in/marquismorrow/)
--- src/components/EntertainmentUI.js
+++ src/components/EntertainmentUI.js
@@ -1,213 +1,198 @@
 import { useState, useEffect } from 'react'
-import {
-    VIEW_HOME,
-    VIEW_MOVIES,
-    VIEW_TV,
-    VIEW_BOOKMARKS
-} from '../views'
+import { VIEW_HOME, VIEW_MOVIES, VIEW_TV, VIEW_BOOKMARKS } from '../views'
 import MediaItemCard from './MediaItemCard'
 import SideNavBar from './SideNavBar'
 import TopNavBar from './TopNavBar'
 
-
 const EntertainmentUI = (props) => {
-    const { data } = props
-    const movies = data.filter(d => d.category === 'Movie')
-    const tv = data.filter(d => d.category === 'TV Series')
-    const bookmarked = data.filter(d => d.isBookmarked)
+	const { data } = props
+	const movies = data.filter((d) => d.category === 'Movie')
+	const tv = data.filter((d) => d.category === 'TV Series')
+	const bookmarked = data.filter((d) => d.isBookmarked)
 
-    const [currentView, setCurrentView] = useState(VIEW_HOME)
-    const [searchState, setSearchState] = useState({
-        input: '',
-        results: []
-    })
-    const [bookmarks, setBookmarks] = useState([...bookmarked])
+	const [currentView, setCurrentView] = useState(VIEW_HOME)
+	const [searchState, setSearchState] = useState({
+		input: '',
+		results: [],
+	})
+	const [bookmarks, setBookmarks] = useState([...bookmarked])
 
-    // SEARCH EFFECTS
-    const { input } = searchState
-    useEffect(() => {
-        let results = []
-        if (input !== '') {
-            if (input.length >= 3) {
-                switch (currentView) {
-                    case VIEW_HOME:
-                        results = handleSearchByName(input, data)
-                        break
-                    case VIEW_MOVIES:
-                        results = handleSearchByName(input, movies)
-                        break
-                    case VIEW_TV:
-                        results = handleSearchByName(input, tv)
-                        break
-                    case VIEW_BOOKMARKS:
-                        results = handleSearchByName(input, bookmarked)
-                        break
-                    default:
-                        results = []
-                }
+	// SEARCH EFFECTS
+	const { input } = searchState
+	useEffect(() => {
+		let results = []
+		if (input !== '') {
+			if (input.length >= 3) {
+				switch (currentView) {
+					case VIEW_HOME:
+						results = handleSearchByName(input, data)
+						break
+					case VIEW_MOVIES:
+						results = handleSearchByName(input, movies)
+						break
+					case VIEW_TV:
+						results = handleSearchByName(input, tv)
+						break
+					case VIEW_BOOKMARKS:
+						results = handleSearchByName(input, bookmarked)
+						break
+					default:
+						results = []
+				}
 
-                setSearchState({
-                    ...searchState,
-                    results: results
-                })
-            }
-        }
-    }, [input])
+				setSearchState({
+					...searchState,
+					results: results,
+				})
+			}
+		}
+	}, [input])
 
-    useEffect(() => {
-        setSearchState({
-            input: '',
-            results: []
-        })
-    }, [currentView])
+	useEffect(() => {
+		setSearchState({
+			input: '',
+			results: [],
+		})
+	}, [currentView])
 
-    const handleSearchByName = (input, data) => {
-        return data.filter(d => {
-            const title = d.title.toLowerCase()
-            return title.includes(input.toLowerCase())
-        })
-    }
+	const handleSearchByName = (input, data) => {
+		return data.filter((d) => {
+			const title = d.title.toLowerCase()
+			return title.includes(input.toLowerCase())
+		})
+	}
 
-    const handleSearchInputChange = (event) => {
-        const { value } = event.target
-        setSearchState({
-            ...searchState,
-            input: value
-        })
-    }
+	const handleSearchInputChange = (event) => {
+		const { value } = event.target
+		setSearchState({
+			...searchState,
+			input: value,
+		})
+	}
 
-    const handleBookmark = (item) => {
-        const isBookmarked = bookmarks.includes(item)
-        item.isBookmarked = !isBookmarked
-        if (!isBookmarked)
-            setBookmarks([...bookmarks, item])
-        else
-            setBookmarks(bookmarks.filter(x => x.title === item.title))
-    }
+	const handleBookmark = (item) => {
+		const isBookmarked = bookmarks.includes(item)
+		item.isBookmarked = !isBookmarked
+		if (!isBookmarked) setBookmarks([...bookmarks, item])
+		else setBookmarks(bookmarks.filter((x) => x.title === item.title))
+	}
 
-    const renderHome = () => {
-        return (
-            <div className="row">
-                <h3 className="text-white mb-3">Recommended for you</h3>
-                {
-                    data.map(item => {
-                        return <MediaItemCard item={item} handleBookmark={handleBookmark} />
-                    })
-                }
-            </div>
-        )
-    }
+	const renderHome = () => {
+		return (
+			<div className='row'>
+				<h3 className='text-white mb-3'>Recommended for you</h3>
+				{data.map((item) => {
+					return <MediaItemCard item={item} handleBookmark={handleBookmark} />
+				})}
+			</div>
+		)
+	}
 
-    const renderMovies = () => {
-        return (
-            <div className="row">
-                <h3 className="text-white mb-3">Movies</h3>
-                {
-                    movies.map(item => {
-                        return <MediaItemCard item={item} handleBookmark={handleBookmark} />
-                    })
-                }
-            </div>
-        )
-    }
+	const renderMovies = () => {
+		return (
+			<div className='row'>
+				<h3 className='text-white mb-3'>Movies</h3>
+				{movies.map((item) => {
+					return <MediaItemCard item={item} handleBookmark={handleBookmark} />
+				})}
+			</div>
+		)
+	}
 
-    const renderTV = () => {
-        return (
-            <div className="row">
-                <h3 className="text-white mb-3">TV Series</h3>
-                {
-                    tv.map(item => {
-                        return <MediaItemCard item={item} handleBookmark={handleBookmark} />
-                    })
-                }
-            </div>
-        )
-    }
+	const renderTV = () => {
+		return (
+			<div className='row'>
+				<h3 className='text-white mb-3'>TV Series</h3>
+				{tv.map((item) => {
+					return <MediaItemCard item={item} handleBookmark={handleBookmark} />
+				})}
+			</div>
+		)
+	}
 
-    const renderBookmarks = () => {
-        const bookmarkedMovies = bookmarked.filter(x => x.category == 'Movie')
-        const bookmarkedTV = bookmarked.filter(x => x.category == 'TV Series')
-        return (
-            <div className="row">
-                <h3 className="text-white mb-3">Bookmarked Movies</h3>
-                {
-                    bookmarkedMovies.map(item => {
-                        return <MediaItemCard item={item} handleBookmark={handleBookmark} />
-                    })
-                }
-                <h3 className="text-white mb-3">Bookmarked TV Series</h3>
-                {
-                    bookmarkedTV.map(item => {
-                        return <MediaItemCard item={item} handleBookmark={handleBookmark} />
-                    })
-                }
-            </div>
-        )
-    }
+	const renderBookmarks = () => {
+		const bookmarkedMovies = bookmarked.filter((x) => x.category == 'Movie')
+		const bookmarkedTV = bookmarked.filter((x) => x.category == 'TV Series')
+		return (
+			<div className='row'>
+				<h3 className='text-white mb-3'>Bookmarked Movies</h3>
+				{bookmarkedMovies.map((item) => {
+					return <MediaItemCard item={item} handleBookmark={handleBookmark} />
+				})}
+				<h3 className='text-white mb-3'>Bookmarked TV Series</h3>
+				{bookmarkedTV.map((item) => {
+					return <MediaItemCard item={item} handleBookmark={handleBookmark} />
+				})}
+			</div>
+		)
+	}
 
-    const renderSearchResults = () => {
-        const { results } = searchState
-        return (
-            <div className="row">
-                <h3 className="text-white mb-3">Found {results.length} results for '{input}'</h3>
-                {
-                    results.map(item => {
-                        return <MediaItemCard item={item} handleBookmark={handleBookmark} />
-                    })
-                }
-            </div>
-        )
-    }
+	const renderSearchResults = () => {
+		const { results } = searchState
+		return (
+			<div className='row'>
+				<h3 className='text-white mb-3'>
+					Found {results.length} results for '{input}'
+				</h3>
+				{results.map((item) => {
+					return <MediaItemCard item={item} handleBookmark={handleBookmark} />
+				})}
+			</div>
+		)
+	}
 
-    const renderSearchInputPlaceholder = (current) => {
-        switch (current) {
-            case VIEW_HOME:
-                return 'Search for movies or TV series'
-            case VIEW_MOVIES:
-                return 'Search for movies'
-            case VIEW_TV:
-                return 'Search for TV series'
-            case VIEW_BOOKMARKS:
-                return 'Search for bookmarked shows'
-        }
-    }
+	const renderSearchInputPlaceholder = (current) => {
+		switch (current) {
+			case VIEW_HOME:
+				return 'Search for movies or TV series'
+			case VIEW_MOVIES:
+				return 'Search for movies'
+			case VIEW_TV:
+				return 'Search for TV series'
+			case VIEW_BOOKMARKS:
+				return 'Search for bookmarked shows'
+			default:
+				return ''
+		}
+	}
 
-    const handleViewChange = (view) => {
-        setCurrentView(view)
-    }
+	const handleViewChange = (view) => {
+		setCurrentView(view)
+	}
 
-    return (
-        <div className="container-fluid p-3" style={{ background: '#10141E', minHeight: '100vh' }}>
-            <div className="row h-100">
-                <SideNavBar currentView={currentView} handleViewChange={handleViewChange} />
-                <TopNavBar currentView={currentView} handleViewChange={handleViewChange} />
-                <div className="col-lg-11 col-md-12 pt-4">
-                    <div className="input-group mb-3">
-                        <span className="input-group-text"><img src="/assets/icon-search.svg" /></span>
-                        <input
-                            value={input}
-                            onChange={handleSearchInputChange}
-                            type="text"
-                            className="form-control"
-                            placeholder={renderSearchInputPlaceholder(currentView)}
-                            style={{ color: '#fff' }} />
-                    </div>
-                    {
-                        (searchState.input !== '' && searchState.results.length > 0)
-                            ?
-                            renderSearchResults()
-                            :
-                            <>
-                                {currentView === VIEW_HOME && renderHome()}
-                                {currentView === VIEW_MOVIES && renderMovies()}
-                                {currentView === VIEW_TV && renderTV()}
-                                {currentView === VIEW_BOOKMARKS && renderBookmarks()}
-                            </>
-                    }
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className='container-fluid p-3' style={{ background: '#10141E', minHeight: '100vh' }}>
+			<div className='row h-100'>
+				<SideNavBar currentView={currentView} handleViewChange={handleViewChange} />
+				<TopNavBar currentView={currentView} handleViewChange={handleViewChange} />
+				<div className='col-lg-11 col-md-12 pt-4'>
+					<div className='input-group mb-3'>
+						<span className='input-group-text'>
+							<img src='/assets/icon-search.svg' />
+						</span>
+						<input
+							value={input}
+							onChange={handleSearchInputChange}
+							type='text'
+							className='form-control'
+							placeholder={renderSearchInputPlaceholder(currentView)}
+							style={{ color: '#fff' }}
+						/>
+					</div>
+					{searchState.input !== '' && searchState.results.length > 0 ? (
+						renderSearchResults()
+					) : (
+						<>
+							{currentView === VIEW_HOME && renderHome()}
+							{currentView === VIEW_MOVIES && renderMovies()}
+							{currentView === VIEW_TV && renderTV()}
+							{currentView === VIEW_BOOKMARKS && renderBookmarks()}
+						</>
+					)}
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default EntertainmentUI
